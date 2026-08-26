bb5c559db2009e6c017c576ff2166ec2b3ac886c
bhupinderjot-singh-rai <bhupinderjot.rai@gmail.com>
2026-08-26 19:00:20 +0530

    Add example configuration template

---
+++ config.example.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_EXAMPLE_H
+#define CONFIG_EXAMPLE_H
+
+// Copy this file to config.h and enter your own credentials.
+
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+#define TELEGRAM_BOT_TOKEN "YOUR_TELEGRAM_BOT_TOKEN"
+#define TELEGRAM_CHAT_ID "YOUR_TELEGRAM_CHAT_ID"
+
+#endif
